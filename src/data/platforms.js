a291c519f1d9d5fee16fcf9c830ff1775b2f5c96
H4ckd <65045995+H4ckd@users.noreply.github.com>
2025-06-04 17:17:16 +0200

    Add platform dropdown and new dashboard sections

---
+++ src/data/platforms.js
@@ -0,0 +1,35 @@
+export const huberwayLinks = [
+  {
+    name: "HubConnect",
+    description: "Centralized management of Sales and marketing",
+    url: "https://app.huberway.com",
+    icons: [
+      "https://dev.huberway.com/icon/sales.svg",
+    ],
+  },
+  {
+    name: "MailMaster",
+    description: "Centralized management of Email Marketing & Automation",
+    url: "https://campaign.huberway.com",
+    icons: [
+      "https://dev.huberway.com/icon/marketing.svg",
+    ],
+  },
+  {
+    name: "SmartChat AI",
+    description: "Support your customers with AI-powered chatbots",
+    url: "#",
+    icons: ["https://dev.huberway.com/icon/smartchat.svg"],
+  },
+  {
+    name: "ContentFlow",
+    description: "Content management, E-Commerce and Web App Development",
+    url: "#",
+    icons: ["https://dev.huberway.com/icon/content.svg"],
+  },
+  {
+    name: "Web Analytics",
+    description: "Detailed analysis of traffic and conversions",
+    url: "https://analytics.huberway.com",
+  },
+];
